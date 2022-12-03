85ef2f30498f2e6e6699f48d6df8fea1d1f9be9f
Nophaie Viscente <nophaieviscente@gmail.com>
2022-12-02 22:30:52 -0300

    Uploading refactoring class EDA and appy PEP8

--- app/core/EDA.py
+++ app/core/EDA.py
@@ -3,105 +3,126 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-class EDA :
-    
+class EDA:
     @staticmethod
-    def plotting_numeric_data (
-    data:pd.DataFrame,
-    numeric_columns:list,
-    title_of_plot:str) :
+    def plotting_numeric_data(
+        data: pd.DataFrame, numeric_columns: list, title_of_plot: str
+    ):
 
-        '''
-            This method plot numeric data in two columns,
-            when one contains a boxplot and other with histplot
+        """
+        This method plot numeric data in two columns,
+        when one contains a boxplot and other with histplot
 
-            Parameters :
-                data : A dataframe to plot numerica data
-                numeric_columns : A previously filtered numeric data columns
-                title_of_plot: A title to all plots    
-        '''
+        Parameters :
+            data : A dataframe to plot numerica data
+            numeric_columns : A previously filtered numeric data columns
+            title_of_plot: A title to all plots
+        """
 
         # Size of figure
-        fig, ax = plt.subplots(figsize=(25,35))
+        fig, ax = plt.subplots(figsize=(25, 35))
         # Wall color
-        color_wall = '#f5f5f5'
+        color_wall = "#f5f5f5"
         fig.set_facecolor(color_wall)
         # Color palette
-        #palette = sns.color_palette('flare', len(numeric_columns)*2)
+        # palette = sns.color_palette('flare', len(numeric_columns)*2)
         # Title
-        plt.suptitle(
-            title_of_plot, 
-            fontsize=22,
-            color='#404040',
-            fontweight=600 )
-        
+        plt.suptitle(title_of_plot, fontsize=22, color="#404040", fontweight=600)
+
         # Structure of plot
         lines = len(numeric_columns)
-        number_of_columns_to_plot = 2 # Boxplot and Histplot
-        position = 1 # Initial position of grid
-        
+        number_of_columns_to_plot = 2  # Boxplot and Histplot
+        position = 1  # Initial position of grid
+
         # Creating plot
-        for column in numeric_columns :
+        for column in numeric_columns:
             # Plot at the grid
             plt.subplot(lines, number_of_columns_to_plot, position)
             # Plot boxplot
             sns.boxplot(data=data, y=column)
             # Change position
-            position+=1
-            #plot at the grid
+            position += 1
+            # plot at the grid
             plt.subplot(lines, number_of_columns_to_plot, position)
             # Plot histplot
-            sns.histplot(data[column],kde=True, stat='density')
+            sns.histplot(data[column], kde=True, stat="density")
             # Change position
-            position+=1
+            position += 1
         # Adjust grid
         plt.subplots_adjust(top=0.95, hspace=0.3)
         return plt.show()
-    
-    @staticmethod
-    def describe_column(
-        dataframe:pd.DataFrame, column:str)->pd.Series:
 
-        print(f'{column} statistics: \n\n{dataframe[column].describe()}')
-    
+    @staticmethod
+    def describe_column(dataframe: pd.DataFrame, filter_: bool = False, **kwargs):
+
+        """
+        This method describe a column using filter or not.
+        In 'filter_' equals True, so the method expects three vars.
+        (column, column_to_filter, filter).
+        Else the method expects just one var. (column)
+
+        Args :
+            column:str = A column to looking for.
+            column_to_filter:str = A column to using filter, if filter true in input.
+            filter:[str|int|float] = A filter to get data describe.
+
+        Ex :
+            If filter: describe_column(dataframe=data, filter_=True,
+            column='Amount', column_to_filter='Class', filter=0)
+
+            No filter(default):describe_column(dataframe=data, column='any_str')
+        """
+        if filter_:
+            return print(
+                f"""{kwargs["column"]} statistics.\nFilter Applied: {kwargs['column_to_filter']} equals {kwargs['filter']}.\n{
+                dataframe[kwargs["column"]]
+                .loc[dataframe[kwargs["column_to_filter"]] == kwargs["filter"]]
+                .describe()}"""
+            )
+
+        return print(
+            f"""{kwargs['column']} statistics:\n{
+                dataframe[kwargs['column']].describe()}"""
+        )
+
     @staticmethod
     def verify_balance_data(
-        dataframe:pd.DataFrame, 
-        column:str, 
-        dict_map_classes:dict,
-        title_plot:str)->sns:
+        dataframe: pd.DataFrame, column: str, dict_map_classes: dict, title_plot: str
+    ) -> sns:
 
         processed_data = dataframe[column].value_counts().sort_index()
 
-        fig, ax = plt.subplots(figsize=(12.5,6))
+        fig, ax = plt.subplots(figsize=(12.5, 6))
 
-        for axis in ['top', 'right', 'left']:
+        for axis in ["top", "right", "left"]:
             ax.spines[axis].set_color(None)
-    
-        for indice, value in enumerate(processed_data) :
+
+        for indice, value in enumerate(processed_data):
             axis_x = dict_map_classes[indice]
-            fig = plt.bar(axis_x,value,width=0.95,edgecolor='white',linewidth=0.95)
-            ax.text(x=axis_x ,y=value, s=value ,horizontalalignment='center',
-                    verticalalignment='bottom',fontdict={'fontsize':12.25})
-        plt.title(title_plot, fontdict={'weight':'semibold', 'fontsize':20})
+            fig = plt.bar(axis_x, value, width=0.95, edgecolor="white", linewidth=0.95)
+            ax.text(
+                x=axis_x,
+                y=value,
+                s=value,
+                horizontalalignment="center",
+                verticalalignment="bottom",
+                fontdict={"fontsize": 12.25},
+            )
+        plt.title(title_plot, fontdict={"weight": "semibold", "fontsize": 20})
 
         return plt.show()
-    
+
     @staticmethod
     def percentage_classes(
-    dataframe:pd.DataFrame,
-    column:str,
-    dict_map_classes:dict)->str:
+        dataframe: pd.DataFrame, column: str, dict_map_classes: dict
+    ) -> str:
 
-        for tuple in dataframe[column].map(
-            dict_map_classes).value_counts().items() :
+        for tuple in dataframe[column].map(dict_map_classes).value_counts().items():
 
-            print(f'Percentage of {tuple[0]}: {round(tuple[1]/len(dataframe)*100, 1)}%')
-    
+            print(f"Percentage of {tuple[0]}: {round(tuple[1]/len(dataframe)*100, 1)}%")
+
     @staticmethod
-    def plot_correlation(
-        dataframe:pd.DataFrame,
-        annot:bool=False)->sns :
+    def plot_correlation(dataframe: pd.DataFrame, annot: bool = False) -> sns:
 
         correlation = dataframe.corr()
         sns.heatmap(data=correlation, annot=annot)
--- app/core/SQLeto.py
+++ app/core/SQLeto.py
@@ -4,125 +4,108 @@ import glob
 import time
 
 
-
-class SQLeto : 
-
+class SQLeto:
     @classmethod
-    def __init__(
-        self, database:str, 
-        user:str, password:str) -> None:
+    def __init__(self, database: str, user: str, password: str) -> None:
 
         self.database = database
         self.user = user
         self.pwd = password
-    
+
     @classmethod
-    def create_engine(
-        self)->sql.engine.Engine :
+    def create_engine(self) -> sql.engine.Engine:
 
         url = f"postgresql://{self.user}:{self.pwd}@localhost:15432/{self.database}"
         engine = sql.create_engine(url)
 
         return engine
-    
+
     @classmethod
-    def connect_database(
-        self, engine)->sql.engine.Connection :
+    def connect_database(self, engine) -> sql.engine.Connection:
 
         return engine.connect()
-    
+
     @classmethod
-    def execute_DQL(
-        self, query:str)->pd.DataFrame :
+    def execute_DQL(self, query: str) -> pd.DataFrame:
 
         return pd.read_sql(query, self.create_engine())
-    
+
     @classmethod
-    def execute_DDL(
-        self, query:str)->str:
+    def execute_DDL(self, query: str) -> str:
 
         conn = self.connect_database(self.create_engine())
         return conn.execute(query)
-    
-    @classmethod
-    def get_dataframes_to_upload(
-        self,
-        path:str) :
 
-        list_paths = glob.glob(
-            path+"*.csv")
+    @classmethod
+    def get_dataframes_to_upload(self, path: str):
+
+        list_paths = glob.glob(path + "*.csv")
         list_names = list()
-        for file_path in list_paths :
-            name_file = file_path.split('\\')[-1]
-            name_dataframe = name_file.replace('.csv','')
-            list_names.append(
-                name_dataframe)
-            
-            globals()[f"df_{name_dataframe}"] = pd.read_csv(
-                path+name_file)
+        for file_path in list_paths:
+            name_file = file_path.split("\\")[-1]
+            name_dataframe = name_file.replace(".csv", "")
+            list_names.append(name_dataframe)
+
+            globals()[f"df_{name_dataframe}"] = pd.read_csv(path + name_file)
 
         return list_names
-    
+
     @classmethod
-    def create_type_column(
-        self,
-        data:pd.DataFrame)->dict :
+    def create_type_column(self, data: pd.DataFrame) -> dict:
 
         type_columns = dict()
-        for column in data.columns :
-            if str(data[column].dtype).startswith('float') :
-                type_columns[column] = 'REAL'
-            elif str(data[column].dtype).startswith('int') :
-                type_columns[column] = 'INTEGER'
-            elif str(data[column].dtype).startswith('datetime') :
-                type_columns[column] = 'DATE'
-            elif str(data[column].dtype).startswith('object') :
-                max_len = int(data[column].str.len().max()+50)
-                if max_len > 1000 :
-                    type_columns[column] = 'TEXT'
+        for column in data.columns:
+            if str(data[column].dtype).startswith("float"):
+                type_columns[column] = "REAL"
+            elif str(data[column].dtype).startswith("int"):
+                type_columns[column] = "INTEGER"
+            elif str(data[column].dtype).startswith("datetime"):
+                type_columns[column] = "DATE"
+            elif str(data[column].dtype).startswith("object"):
+                max_len = int(data[column].str.len().max() + 50)
+                if max_len > 1000:
+                    type_columns[column] = "TEXT"
                     continue
-                type_columns[column] = f'VARCHAR({max_len})'
-                
+                type_columns[column] = f"VARCHAR({max_len})"
+
         return type_columns
-    
+
     @classmethod
     def creating_tables(
         self,
-        dataframe:pd.DataFrame, 
-        name_table:str,  
-        primary_key:str, 
-        has_fk:bool=False,
-        foreign_key:str='',
-        reference_table:str=''):
+        dataframe: pd.DataFrame,
+        name_table: str,
+        primary_key: str,
+        has_fk: bool = False,
+        foreign_key: str = "",
+        reference_table: str = "",
+    ):
 
-        query = f'CREATE TABLE {name_table}('
+        query = f"CREATE TABLE {name_table}("
         schema = self.create_type_column(data=dataframe)
-        for key, value in schema.items() :
-            query+= f'{key} {value},'
-        if has_fk :
-                query+=f'FOREIGN KEY ({foreign_key}) REFERENCES {reference_table} ({foreign_key}),'
-        query+= f'PRIMARY KEY ({primary_key}));'
+        for key, value in schema.items():
+            query += f"{key} {value},"
+        if has_fk:
+            query += f"FOREIGN KEY ({foreign_key}) REFERENCES {reference_table} ({foreign_key}),"
+        query += f"PRIMARY KEY ({primary_key}));"
 
         return query
-    
+
     @classmethod
     def send_dataframes_to_tables(
-        self,
-        list_names_order:list,
-        if_exists_:str)->None:
+        self, list_names_order: list, if_exists_: str
+    ) -> None:
 
-        '''
-            This method receives a path of dataframes, so send it to tables in database.
-        '''
+        """
+        This method receives a path of dataframes, so send it to tables in database.
+        """
 
         names = list_names_order
-        for name in names :
+        for name in names:
             print(f"Saving table {name}")
             globals()[f"df_{name}"].to_sql(
-                name, 
-                self.create_engine(), 
-                if_exists=if_exists_, 
-                index=False)
+                name, self.create_engine(), if_exists=if_exists_, index=False
+            )
             time.sleep(0.8)
             print(f"Table {name} saved!")
             print()
