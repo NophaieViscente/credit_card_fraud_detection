8366f72b3382e4252ba4fd1755bac2d7953e41cf
Nophaie Viscente <nophaieviscente@gmail.com>
2022-12-16 23:32:13 -0300

    Uploading new pipeline

--- app/core/PipelineProject.py
+++ app/core/PipelineProject.py
@@ -1,13 +1,7 @@
 import pandas as pd
-from collections import defaultdict
-from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
-from sklearn.metrics import (
-    accuracy_score,
-    recall_score,
-    f1_score,
-    precision_score,
-    roc_auc_score,
-)
+import pickle
+from sklearn.model_selection import train_test_split, cross_validate, GridSearchCV
+from sklearn import preprocessing
 
 
 class PrepareDataAndTrainingModels:
@@ -18,10 +12,10 @@ class PrepareDataAndTrainingModels:
         is_only_numeric_cols: bool = True,
         random_state: int = 42,
         test_size: float = 0.3,
-        balance: bool = False,
         **kwargs,
     ) -> None:
 
+        self.save_path = "models/"
         self.dataframe = dataframe
         self.target = target
         self.is_only_num_cols = is_only_numeric_cols
@@ -32,134 +26,123 @@ class PrepareDataAndTrainingModels:
         self.X_test = None
         self.Y_test = None
         self.fitted_models = None
-        self.balance = balance
+        self.models_predictions = None
         self.kwargs = kwargs
 
-    def get_name_cat_and_num_cols(self) -> None:
-        """
-        This method get names of columns categorical and numerical columns.
-        """
-        if self.is_only_num_cols:
-            return (
-                type(self.dataframe[self.dataframe.columns].columns),
-                self.dataframe[self.dataframe.columns]
-                .drop(self.target, axis=1)
-                .columns,
-            )
+    @staticmethod
+    def persist_model(
+        model,
+        save_path: str,
+    ) -> None:
+        with open(save_path, "wb") as file:
+            pickle.dump(model, file)
 
-        possible_cat_cols = list()
-        possible_cat_cols.append(self.target)
-        for i, type_ in enumerate(self.dataframe.dtypes):
-            if type_ == object:
-                possible_cat_cols.append(self.dataframe.iloc[:, i].name)
-            elif type_ == pd.CategoricalDtype.name:
-                possible_cat_cols.append(self.dataframe.iloc[:, i].name)
+    @staticmethod
+    def load_model(load_path: str):
+        with open(load_path, "rb") as file:
+            model = pickle.load(file)
+        return model
 
-        return type(
-            (
-                self.dataframe[possible_cat_cols].columns,
-                self.dataframe.drop(possible_cat_cols, axis=1).columns,
-            )
-        ), (
-            self.dataframe[possible_cat_cols].columns,
-            self.dataframe.drop(possible_cat_cols, axis=1).columns,
-        )
-
-    def splitting_data(self, **kwargs) -> None:
+    def splitting_data(self, standart_scale=False, **kwargs) -> None:
         X = self.dataframe.drop(self.target, axis=1)
+        if standart_scale == True:
+            X = preprocessing.StandardScaler().fit(X).transform(X)
+
         Y = self.dataframe[self.target]
         self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(
             X, Y, random_state=self.random_state, test_size=self.test_size, **kwargs
         )
 
     def balance_data(self):
+        qt_classes = (
+            self.dataframe[self.target]
+            .value_counts(ascending=True)
+            .reset_index(drop=True)
+        )
+        if len(qt_classes) == 2:
+            print("Its Binary Classification!\n\n")
+            print("#" * 50 + "\n\n")
 
-        print("#" * 50 + "\n\n")
-        if self.balance == True:
-            print(f"Using tecnique: {self.kwargs['balancer']}")
-            self.X_train, self.Y_train = self.kwargs["balancer"].fit_resample(
-                self.X_train,
-                self.Y_train,
-            )
-        return self.X_train, self.X_test, self.Y_train, self.Y_test
+            if (self.kwargs["balancer"] != None) and (
+                qt_classes[0] / qt_classes[1] < 0.35
+            ):
+                print(f"Using tecnique: {self.kwargs['balancer']}")
+                self.X_train, self.Y_train = self.kwargs["balancer"].fit_resample(
+                    self.X_train,
+                    self.Y_train,
+                )
 
     def pre_process_data(self):
+        if not self.X_train:
+            self.splitting_data()
+
         if self.is_only_num_cols:
-            self.X_train, self.X_test, self.Y_train, self.Y_test = self.balance_data()
+            self.balance_data()
             self.X_train = self.kwargs["scaler"].fit_transform(self.X_train)
             self.X_test = self.kwargs["scaler"].fit_transform(self.X_test)
 
-    def cross_validation(self, predictor, **kwargs):
-        return cross_val_score(predictor, self.X_train, self.Y_train, **kwargs)
-
-    def fit_models(self, grid_search_cv=False, param=None, cv=5, n_jobs=-1) -> None:
+    def fit_models(
+        self,
+        cross_val=False,
+        grid_search_cv=False,
+        persist=False,
+        param=None,
+        cv=5,
+        n_jobs=-1,
+    ) -> pd.DataFrame:
         models = self.kwargs["models"]
-        fitted_models = defaultdict(list)
+        fitted_models = dict()
         for _, model in enumerate(models):
-            predictor = model
             if grid_search_cv == True:
                 if param == None:
                     raise AttributeError(
                         f"If grid_search_cv is True, param cannot be None. param = {param}."
                     )
-                predictor = GridSearchCV(predictor, param, cv, n_jobs)
+
+                predictor = GridSearchCV(
+                    estimator=model, param_grid=param, cv=cv, n_jobs=n_jobs
+                )
                 predictor.fit(self.X_train, self.Y_train)
-                fitted_models["Model"].append(predictor.best_estimator_)
+
+            elif cross_val == True:
+                predictor = cross_validate(
+                    estimator=model,
+                    X=self.X_train,
+                    y=self.Y_train,
+                    cv=cv,
+                    n_jobs=n_jobs,
+                )
 
             else:
-                predictor.fit(self.X_train, self.Y_train)
-                fitted_models["Model"].append(predictor)
+                predictor = model.fit(self.X_train, self.Y_train)
 
-        self.fitted_models = pd.DataFrame(fitted_models)
+            if persist == True:
+                self.persist_model(predictor, f"{self.save_path}+{str(model)}.pkl")
+            fitted_models[str(model)] = predictor
 
-    def fit_models_cv(self) -> None:
-        models = self.kwargs["models"]
-        fitted_models = dict()
-
-        for _, model in enumerate(models):
-            predictor = model
-            scores_ = self.cross_validation(
-                predictor, cv=5, scoring=self.kwargs["score_metric"]
-            )
-            print(f"Metric: {self.kwargs['score_metric']} - {scores_.mean()}")
-
-        self.fitted_models = (
-            pd.DataFrame(fitted_models)
-            .T.reset_index()
-            .rename(columns={"index": "model"})
-        )
+        self.fitted_models = fitted_models
 
     def predict(self) -> pd.DataFrame:
         models = self.kwargs["models"]
         predictions = dict()
         for _, model in enumerate(models):
-            predictor = model
-            prediction = predictor.predict(self.X_test, self.Y_test)
-            predictions[str(model)] = prediction
+            predictions[str(model)] = model.predict(self.X_test, self.Y_test)
 
-        return (
-            pd.DataFrame(prediction).T.reset_index().rename(columns={"index": "model"})
-        )
+        self.models_predictions = predictions
 
     def compute_scores(self, **kwargs) -> pd.DataFrame:
         score_models = dict()
         models = self.kwargs["models"]
+        metrics = self.kwargs["score_metric"]
         for _, model in enumerate(models):
             score_models[str(model)] = dict()
             predictor = model
             prediction = predictor.predict(self.X_test)
-            score_models[str(model)]["accuracy"] = accuracy_score(
-                self.Y_test, prediction
-            )
-            score_models[str(model)]["precision"] = precision_score(
-                self.Y_test, prediction
-            )
-            score_models[str(model)]["recall"] = recall_score(self.Y_test, prediction)
-            score_models[str(model)]["f1_score"] = f1_score(self.Y_test, prediction)
-            score_models[str(model)]["roc_auc_score"] = roc_auc_score(
-                self.Y_test, prediction
-            )
+            for _, metric in enumerate(metrics):
+                score_models[str(model)][metric] = metric(self.Y_test, prediction)
 
         return (
-            pd.DataFrame(score_models).reset_index().rename(columns={"index": "model"})
+            pd.DataFrame(score_models)
+            .T.reset_index()
+            .rename(columns={"index": "model"})
         )
